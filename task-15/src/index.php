a37b641b8ae32d8c11691c472648c4146229a345
dashakuh22 <dashakuh14@gmail.com>
2022-07-28 15:35:45 +0300

    fix autoload

--- task-15/src/index.php
+++ task-15/src/index.php
@@ -7,7 +7,7 @@ function file_build_path(...$segments)
 
 const ROOT = __DIR__;
 
-require_once file_build_path(ROOT, '..', '..', 'vendor', 'autoload.php');
+require_once file_build_path(ROOT, '..', 'vendor', 'autoload.php');
 require_once file_build_path(ROOT, 'components', 'Router.php');
 
 $router = new Router();
