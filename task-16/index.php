84e8d1528d20bd05251e24b40dec4bb5432b1418
dashakuh22 <dashakuh14@gmail.com>
2022-08-03 12:20:21 +0300

    move files from src

--- task-16/index.php
+++ task-16/index.php
@@ -1,13 +1,13 @@
 <?php
 
-const ROOT = __DIR__;
-
 function file_build_path(...$segments): string
 {
     return implode(DIRECTORY_SEPARATOR, $segments);
 }
 
-require_once file_build_path(ROOT, '..', 'vendor', 'autoload.php');
+const ROOT = __DIR__;
+
+require_once file_build_path(ROOT, 'vendor', 'autoload.php');
 require_once file_build_path(ROOT, 'components', 'Router.php');
 
 $router = new Router();
