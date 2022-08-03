7b06bc3ac611fb860db3b34dd04d5388a1c45b25
dashakuh22 <dashakuh14@gmail.com>
2022-08-03 17:31:52 +0300

    add password hash

--- task-16/config/configUsers.php
+++ task-16/config/configUsers.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-const ALGO = PASSWORD_BCRYPT;
-
 return [
     'user1@test.com' => [
         'name' => 'John',
-        'password' => password_hash('your_hash_here1', ALGO),
+        'password' => '$2y$10$Ktw1rnxOsHlPqkIBDnqYEO3FclbX3ma4yMt2dP4G34lZYtyEKvCry',
     ],
     'user2@test.com' => [
         'name' => 'Jane',
-        'password' => password_hash('your_hash_here2', ALGO),
+        'password' => '$2y$10$kAyxt9dkh6x2E3DYbRuwLekqOLt9Cy/1sLvPciaG75gQHUR7vuM0C',
     ],
 ];
